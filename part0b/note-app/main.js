50d9983577dcc94bd6012b43dfea6c67b3647d5f
dan7sc <su7cheng@gmail.com>
2020-04-18 12:06:21 -0300

    Add script to make ajax request

---
+++ part0b/note-app/main.js
@@ -0,0 +1,20 @@
+const xhttp =new XMLHttpRequest();
+xhttp.onreadystate = () => {
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
+        const data = JSON.parse(xhttp.response);
+        console.log(data);
+
+        const ul =document.createElement('ul');
+        us.setAttribute('class', 'notes');
+
+        data.forEach(note => {
+            const li = document.createElement('li');
+            ul.appendChild(li);
+            li.appendChild(document.createTextNode(note.content));
+        });
+
+        document.getElementById('notes').appendChild(ul);
+    }
+};
+xhttp.open('GET', '/data.json', true);
+xhttp.send();
