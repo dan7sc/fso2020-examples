1eb157d6dabbefe70daff981b01977fbea98044d
dan7sc <su7cheng@gmail.com>
2020-04-25 12:22:37 -0300

    part3#server# Server receive data from client

--- part3/index.js
+++ part3/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 let notes = require('./db.json')
 
+const PORT = 3001
 const app = express()
 
-const PORT = 3001
+app.use(express.json())
 
 app.get('/api/notes', (req, res) => {
     res.json(notes)
@@ -16,6 +17,21 @@ app.get('/api/notes/:id', (req, res) => {
     res.status(404).end()
 })
 
+app.post('/api/notes', (req, res) => {
+    const body = req.body
+    if (!body.content) {
+        return res.status(400).json({error: 'content missing'})
+    }
+    const newNote = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId()
+    }
+    notes = notes.concat(newNote)
+    res.json(newNote)
+})
+
 app.delete('/api/notes/:id', (req, res) => {
     const id = Number.parseInt(req.params.id)
     notes = notes.filter(note => note.id !== id)
@@ -25,3 +41,10 @@ app.delete('/api/notes/:id', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+const generateId = () => {
+    const maxId = notes.length > 0
+          ? Math.max(...notes.map(note => note.id))
+          : 0
+    return maxId + 1
+}
