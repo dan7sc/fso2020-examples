21ef92a846590723a3584ff3d9cc2a1259e6fc5e
dan7sc <su7cheng@gmail.com>
2020-04-25 12:05:48 -0300

    part3#server# Fetch a single resource

--- part3/index.js
+++ part3/index.js
@@ -9,6 +9,13 @@ app.get('/api/notes', (req, res) => {
     res.json(notes)
 })
 
+app.get('/api/notes/:id', (req, res) => {
+    const id = Number.parseInt(req.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) return res.json(note)
+    res.status(404).end()
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
