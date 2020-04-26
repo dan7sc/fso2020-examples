7d71612ab056d8a8a5bd3a4d4696004226d3d93b
dan7sc <su7cheng@gmail.com>
2020-04-26 13:03:31 -0300

    part3#database# Use database in route handlers

--- part3/index.js
+++ part3/index.js
@@ -18,46 +18,39 @@ app.get('/api/notes', (req, res) => {
 })
 
 app.get('/api/notes/:id', (req, res) => {
-    const id = Number.parseInt(req.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) return res.json(note)
-    res.status(404).end()
+    const id = req.params.id
+    Note.findById(id).then(note => {
+        res.json(note.toJSON())
+    })
 })
 
 app.post('/api/notes', (req, res) => {
     const body = req.body
-    if (!body.content) {
+    if (body.content === undefined) {
         return res.status(400).json({error: 'content missing'})
     }
-    const newNote = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date(),
-        id: generateId()
-    }
-    notes = notes.concat(newNote)
-    res.json(newNote)
+        date: new Date()
+    })
+    note.save().then(savedNote => {
+        res.json(savedNote.toJSON())
+    })
 })
 
-app.put('/api/notes/:id', (req, res) => {
-    const returnedNote = req.body
-    notes = notes.map(note => note.id !== returnedNote.id ? note : returnedNote)
-    res.json(returnedNote)
-})
+// app.put('/api/notes/:id', (req, res) => {
+//     const returnedNote = req.body
+//     notes = notes.map(note => note.id !== returnedNote.id ? note : returnedNote)
+//     res.json(returnedNote)
+// })
 
-app.delete('/api/notes/:id', (req, res) => {
-    const id = Number.parseInt(req.params.id)
-    notes = notes.filter(note => note.id !== id)
-    res.status(204).end()
-})
+// app.delete('/api/notes/:id', (req, res) => {
+//     const id = Number.parseInt(req.params.id)
+//     notes = notes.filter(note => note.id !== id)
+//     res.status(204).end()
+// })
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
-
-const generateId = () => {
-    const maxId = notes.length > 0
-          ? Math.max(...notes.map(note => note.id))
-          : 0
-    return maxId + 1
-}
