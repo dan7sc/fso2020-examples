03a29d23e9b0d667ade8cb0077026c6d01d2ec97
dan7sc <su7cheng@gmail.com>
2020-04-27 15:48:40 -0300

    part3#database# Add lint configuration

--- part3/index.js
+++ part3/index.js
@@ -11,76 +11,76 @@ app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
 
-app.get('/api/notes', (req, res) => {
-    Note.find({})
-        .then(notes => {
-            res.json(notes.map(note => note.toJSON()))
-        })
-        .catch(error => next(error))
+app.get('/api/notes', (req, res, next) => {
+  Note.find({})
+    .then(notes => {
+      res.json(notes.map(note => note.toJSON()))
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/notes/:id', (req, res, next) => {
-    const id = req.params.id
-    Note.findById(id)
-        .then(note => {
-            if (note) res.json(note.toJSON())
-            else res.status(404).end()
-        })
-        .catch(error => next(error))
+  const id = req.params.id
+  Note.findById(id)
+    .then(note => {
+      if (note) res.json(note.toJSON())
+      else res.status(404).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (req, res, next) => {
-    const body = req.body
-    if (body.content === undefined) {
-        return res.status(400).json({error: 'content missing'})
-    }
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-        date: new Date()
+  const body = req.body
+  if (body.content === undefined) {
+    return res.status(400).json({ error: 'content missing' })
+  }
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date()
+  })
+  note.save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => {
+      res.json(savedAndFormattedNote)
     })
-    note.save()
-        .then(savedNote => savedNote.toJSON())
-        .then(savedAndFormattedNote => {
-            res.json(savedAndFormattedNote)
-        })
-        .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (req, res, next) => {
-    const body = req.body
-    const id = req.params.id
-    const note = {
-        content: body.content,
-        important: body.important
-    }
-    Note.findByIdAndUpdate(id, note, {new: true})
-        .then(updatedNote => {
-            res.json(updatedNote.toJSON())
-        })
-        .catch(error => next(error))
+  const body = req.body
+  const id = req.params.id
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+  Note.findByIdAndUpdate(id, note, { new: true })
+    .then(updatedNote => {
+      res.json(updatedNote.toJSON())
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (req, res, next) => {
-    const id = req.params.id
-    Note.findByIdAndRemove(id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(error => next(error))
+  const id = req.params.id
+  Note.findByIdAndRemove(id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, req, res, next) => {
-    console.log(error.message)
-    if (error.name === 'CastError') {
-        return res.status(400).send({error: 'malformatted id'})
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).send({error: error.message})
-    }
-    next(error)
+  console.log(error.message)
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).send({ error: error.message })
+  }
+  next(error)
 }
 app.use(errorHandler)
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- part3/mongo.js
+++ part3/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -11,48 +11,48 @@ const url =
       `mongodb+srv://fshelyui:${password}@cluster0-rvapt.mongodb.net/note-app?retryWrites=true&w=majority`
 
 const dbOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 }
 
 mongoose.connect(url, dbOptions)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean
+  content: String,
+  date: Date,
+  important: Boolean
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
 // const notes = [
-//     new Note({
-//         content: 'HTML is easy',
-//         date: new Date(),
-//         important: true
-//     }),
-//     new Note({
-//         content: 'Mongoose makes use of mongo easy',
-//         date: new Date(),
-//         important: true
-//     }),
-//     new Note({
-//         content: 'Callback-functions suck',
-//         date: new Date(),
-//         important: true
-//     })
+//   new Note({
+//     content: 'HTML is easy',
+//     date: new Date(),
+//     important: true
+//   }),
+//   new Note({
+//     content: 'Mongoose makes use of mongo easy',
+//     date: new Date(),
+//     important: true
+//   }),
+//   new Note({
+//     content: 'Callback-functions suck',
+//     date: new Date(),
+//     important: true
+//   })
 // ]
 
 // notes.forEach(n => {
-//     n.save().then(result => {
-//         console.log('note saved')
-//          mongoose.connection.close()
-//     })
+//   n.save().then(() => {
+//     console.log('note saved')
+//     mongoose.connection.close()
+//   })
 // })
 
 Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
 })
