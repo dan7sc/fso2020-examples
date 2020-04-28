08c5827a739be9687a1818218374b863f3a3a535
dan7sc <su7cheng@gmail.com>
2020-04-28 12:50:32 -0300

    part4#test# Refactor tests

--- part3/tests/test_helper.js
+++ part3/tests/test_helper.js
@@ -14,7 +14,10 @@ const initialNotes = [
 ]
 
 const nonExistingId = async () => {
-  const note = new Note({ content: 'willremovethissoon' })
+  const note = new Note({
+    content: 'willremovethissoon',
+    date: new Date()
+  })
   await note.save()
   await note.remove()
   return note._id.toString()
