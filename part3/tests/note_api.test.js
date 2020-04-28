2c5d5a0fc683dc3aa1b862a3449b61f3e41fa738
dan7sc <su7cheng@gmail.com>
2020-04-28 14:31:43 -0300

    part4#test# Add mongoose schema for users

--- part3/tests/note_api.test.js
+++ part3/tests/note_api.test.js
@@ -3,7 +3,9 @@ const supertest = require('supertest')
 const helper = require('../tests/test_helper')
 const app = require('../app')
 const api = supertest(app)
+const bcrypt = require('bcrypt')
 const Note = require('../models/note')
+const User = require('../models/user')
 
 beforeEach(async () => {
   await Note.deleteMany({})
@@ -56,8 +58,6 @@ describe('viewing a specific note', () => {
       .expect(200)
       .expect('Content-Type', /application\/json/)
 
-    delete noteToView.date
-    delete resultNote.body.date
     expect(resultNote.body).toEqual(noteToView)
   })
 
@@ -137,6 +137,64 @@ describe('deletion of a note', () => {
     const contents = notesAtEnd.map(res => res.content)
 
     expect(contents).not.toContain(noteToDelete.content)
+  })
+})
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({
+      username: 'root',
+      passwordHash
+    })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen'
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(user => user.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation fails with proper status code and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen'
+    }
+
+    const result = await
+    api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('`username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
   })
 })
 
--- part3/tests/test_helper.js
+++ part3/tests/test_helper.js
@@ -1,22 +1,20 @@
 const Note = require('../models/note')
+const User = require('../models/user')
 
 const initialNotes = [
   {
     content: 'HTML is easy',
-    date: new Date(),
     important: false
   },
   {
     content: 'Browser can execute only Javascript',
-    date: new Date(),
     important: true
   }
 ]
 
 const nonExistingId = async () => {
   const note = new Note({
-    content: 'willremovethissoon',
-    date: new Date()
+    content: 'willremovethissoon'
   })
   await note.save()
   await note.remove()
@@ -28,8 +26,14 @@ const notesInDb = async () => {
   return notes.map(note => note.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(user => user.toJSON())
+}
+
 module.exports = {
   initialNotes,
   nonExistingId,
-  notesInDb
+  notesInDb,
+  usersInDb
 }
