546207a2a6b3ae1a699364a6dec1f4f3b05420fc
dan7sc <su7cheng@gmail.com>
2020-05-07 15:06:05 -0300

    part6#redux# Separate app in various components

--- part6/note-app/src/App.jsx
+++ part6/note-app/src/App.jsx
@@ -1,93 +1,15 @@
 import React from 'react'
-import { createStore } from 'redux'
-import './App.css'
-import noteReducer from './reducers/noteReducer'
-
-const store = createStore(noteReducer)
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'the app state in the redux store',
-    important: true,
-    id: 1
-  },
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
-
-store.dispatch({
-  type: 'TOGGLE_IMPORTANCE',
-  data: {
-    id: 2
-  }
-})
-
-const generateId = () => {
-  return Number(Math.random() * 1000000).toFixed(0)
-}
-
-const createNote = (content) => {
-  return {
-    type: 'NEW_NOTE',
-    data: {
-      content,
-      important: false,
-      id: generateId()
-    }
-  }
-}
-
-const toggleImportanceOf = (id) => {
-  return {
-    type: 'TOGGLE_IMPORTANCE',
-    data: { id }
-  }
-}
+import NewNote from './components/NewNote'
+import Notes from './components/Note'
 
 function App() {
-  const addNote = event => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-
-    store.dispatch(createNote(content))
-  }
-
-  const toggleImportance = (id) => {
-    store.dispatch(toggleImportanceOf(id))
-  }
-
   return (
     <div>
       <h1>Note App</h1>
-      <form onSubmit={addNote}>
-        <input name='note'/>
-        <button type='submit'>add</button>
-      </form>
-      <ul>
-        {
-          store.getState().map(note => {
-            return (
-              <li
-                key={note.id}
-                onClick={() => toggleImportance(note.id)}
-              >
-                {note.content} <strong>{note.important ? 'important' : ''}</strong>
-              </li>
-            )
-          })
-        }
-      </ul>
+      <NewNote/>
+      <Notes />
     </div>
   )
 }
 
-export default { App, store }
+export default App
