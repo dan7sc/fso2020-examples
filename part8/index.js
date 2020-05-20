a21b0e411e5049123665073ae02ca9abfe86471b
dan7sc <su7cheng@gmail.com>
2020-05-20 16:21:35 -0300

    part8#graphql# Filter query returning persons with the parameter phone

--- part8/index.js
+++ part8/index.js
@@ -48,15 +48,28 @@ type Mutation {
 
 type Query {
   personCount: Int!
-  allPersons: [Person!]!
+  allPersons(phone: YesNo): [Person!]!
   findPerson(name: String!): Person
 }
-`;
+
+enum YesNo {
+  YES
+  NO
+}
+`
 
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if (!args.phone) {
+        return persons
+      }
+      const byPhone = (person) => (
+        args.phone === 'YES' ? person.phone : !person.phone
+      )
+      return persons.filter(byPhone)
+    },
     findPerson: (root, args) => (
       persons.find(p => p.name === args.name)
     )
