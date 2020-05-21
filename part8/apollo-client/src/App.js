957acdc476064248d7fa4412bc6ded93152881ea
dan7sc <su7cheng@gmail.com>
2020-05-21 14:07:24 -0300

    par8#graphql# Inform about an error with a notification

--- part8/apollo-client/src/App.js
+++ part8/apollo-client/src/App.js
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client'
 import { ALL_PERSONS } from './queries'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
   const result = useQuery(ALL_PERSONS)
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
+  const notify = message => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+  }
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
+    </div>
+  )
+}
+
+const Notify = ({ errorMessage }) => {
+  const style = {
+    background: 'red',
+    padding: 8
+  }
+
+  if (!errorMessage) {
+    return null
+  }
+
+  return(
+    <div style={style}>
+      {errorMessage}
     </div>
   )
 }
