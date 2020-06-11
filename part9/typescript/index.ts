9e847538c4a425d9083f6d59226bbe7c331c821c
dan7sc <su7cheng@gmail.com>
2020-06-11 20:19:11 -0300

    part9#typescript# Start by creating a ping-endpoint

--- part9/typescript/index.ts
+++ part9/typescript/index.ts
@@ -1,22 +1,12 @@
-import { calculator } from './src/calculator';
 import express from 'express';
 const app = express();
 
+const PORT = 3003;
+
 app.get('/ping', (_req, res) => {
+    console.log('someone pinged here');
     res.send('pong');
 });
-
-app.get('/calculate', (req, res) => {
-    const { value1, value2, op } = req.query;
-
-    const a = Number(value1);
-    const b = Number(value2);
-
-    const result = calculator(a, b, op);
-    res.send(result.toString());
-});
-
-const PORT = 3003;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
